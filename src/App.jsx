820f55cb58f752efa45c7733a60ea440b56c665f
roycechua23 <royce236@gmail.com>
2023-08-11 03:16:39 +0800

    Add CSS for navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import MenuIcon from '@mui/icons-material/Menu';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<>
+			<main>
+				<div className="navbar">
+					<div className="navbar-menu-bar">
+						<MenuIcon
+							color="whitesmoke"
+							onClick={() => {
+								console.log('sidebar pressed');
+							}}
+							size={16}
+						/>
+					</div>
+					<ul className="navbar-list">
+						<li>TaskLister</li>
+					</ul>
+				</div>
+			</main>
+		</>
+	);
 }
 
-export default App
+export default App;
