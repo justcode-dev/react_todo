0cd9ed8df75a702092b8c13eac183c1b6d5d5039
roycechua23 <royce236@gmail.com>
2023-08-24 01:24:33 +0800

    Separate TodoItem and Todos

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,35 @@
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+function TodoItem() {
+	return (
+		<div className="todo-item-container">
+			<div className="todo-item-container-content">
+				<input type="checkbox" />
+				<div className="todo-item-container-content-text">
+					<p>Do something</p>
+				</div>
+			</div>
+			<div className="todo-item-container-action">
+				<EditIcon
+					sx={{ color: 'warning.light', marginRight: '10px' }}
+					cursor="pointer"
+					onClick={() => {
+						console.log('edit button pressed');
+					}}
+					size={16}
+				/>
+				<DeleteIcon
+					color="error"
+					cursor="pointer"
+					onClick={() => {
+						console.log('delete button pressed');
+					}}
+					size={16}
+				/>
+			</div>
+		</div>
+	);
+}
+
+export default TodoItem;
--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,35 +1,10 @@
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteIcon from '@mui/icons-material/Delete';
+import TodoItem from "./TodoItem";
 
 function Todos() {
 	return (
 		<div className="todos-container">
-			<div className="todo-item-container">
-				<div className="todo-item-container-content">
-					<input type="checkbox" />
-					<div className="todo-item-container-content-text">
-						<p>Do something</p>
-					</div>
-				</div>
-				<div className="todo-item-container-action">
-					<EditIcon
-						sx={{ color: 'warning.light', marginRight: '10px' }}
-						cursor="pointer"
-						onClick={() => {
-							console.log('edit button pressed');
-						}}
-						size={16}
-					/>
-					<DeleteIcon
-						color="error"
-						cursor="pointer"
-						onClick={() => {
-							console.log('delete button pressed');
-						}}
-						size={16}
-					/>
-				</div>
-			</div>
+      <TodoItem/>
+      <TodoItem/>
 		</div>
 	);
 }
