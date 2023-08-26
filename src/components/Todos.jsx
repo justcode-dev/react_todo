6bf60ea57d369f73109782db01a23bb5f2c9493a
roycechua23 <royce236@gmail.com>
2023-08-27 02:05:02 +0800

    Conditional rendering for empty list

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,20 +1,42 @@
 import AddTodo from './AddTodo';
 import TodoItem from './TodoItem';
 
+function NoTodosMessage() {
+	return (
+		<div
+			style={{
+				display: 'flex',
+				flex: 1,
+				flexDirection: 'column',
+				alignItems: 'center',
+				justifyContent: 'flex-start',
+			}}
+		>
+			<p>You have no Todos</p>
+		</div>
+	);
+}
+
 function Todos() {
-	// create mock data
-	const todos = [
-		{ id: 1, todo: 'Feed the dog' },
-		{ id: 2, todo: 'Wash dishes' },
-		{ id: 3, todo: 'Cook breakfast' },
-	];
+	// create mock dataF
+	// const todos = [
+	// 	{ id: 1, todo: 'Feed the dog' },
+	// 	{ id: 2, todo: 'Wash dishes' },
+	// 	{ id: 3, todo: 'Cook breakfast' },
+	// ];
+	const todos = [];
+
+	const TodosList =
+		todos.length > 0 ? (
+			todos.map((todo) => <TodoItem key={todo.id} item={todo} />)
+		) : (
+			<NoTodosMessage />
+		);
 
 	return (
 		<div className="todos-container">
 			<AddTodo />
-			{todos.map((todo) => (
-				<TodoItem key={todo.id} item={todo} />
-			))}
+			{TodosList}
 		</div>
 	);
 }
