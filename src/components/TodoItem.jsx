f0556bce998258eba155cd36e3a13f49af210018
roycechua23 <royce236@gmail.com>
2023-08-28 04:09:08 +0800

    Finish update functionality

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,37 +1,102 @@
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
+import CheckIcon from '@mui/icons-material/Check';
+import ClearIcon from '@mui/icons-material/Clear';
+import { useState } from 'react';
 
 function TodoItem(props) {
 	const item = props.item;
 	const id = item.id;
 	const todo = item.todo;
 	// const { item, id } = props.item;
+
+	const [inEditMode, setInEditMode] = useState(false);
+	const [newTodo, setNewTodo] = useState(todo);
+
+	const handleChange = (e) => {
+		setNewTodo(e.target.value);
+	};
+
+	const handleUpdateTodo = () => {
+		props.onUpdateTodo({
+			id,
+			todo: newTodo,
+		});
+		setInEditMode(false);
+	};
+
+	const handleCancelUpdate = () => {
+		setInEditMode(false);
+	};
+
 	return (
 		<div className="todo-item-container">
-			<div className="todo-item-container-content">
-				<input type="checkbox" />
-				<div className="todo-item-container-content-text">
-					<p>{todo}</p>
+			{inEditMode ? (
+				<div className="add-todo-container">
+					<input
+						value={newTodo}
+						onChange={handleChange}
+						type="text"
+						className="todo-item-add-textinput"
+						placeholder="Add a todo"
+						onKeyDown={(e) => {
+							if (e.key === 'Enter') {
+								handleUpdateTodo();
+							} else if (e.key === 'Escape') {
+								setInEditMode(false);
+							}
+						}}
+					/>
 				</div>
-			</div>
-			<div className="todo-item-container-action">
-				<EditIcon
-					sx={{ color: 'warning.light' }}
-					cursor="pointer"
-					onClick={() => {
-						console.log('edit button pressed');
-					}}
-					size={16}
-				/>
-				<DeleteIcon
-					color="error"
-					cursor="pointer"
-					onClick={() => {
-						props.onDeleteTodo(id)
-					}}
-					size={16}
-				/>
-			</div>
+			) : (
+				<div className="todo-item-container-content">
+					<input type="checkbox" />
+					<div
+						className="todo-item-container-content-text"
+						onClick={() => {
+							setInEditMode(true);
+						}}
+					>
+						<p>{todo}</p>
+					</div>
+				</div>
+			)}
+			{inEditMode ? (
+				<div className="todo-item-container-action">
+					<CheckIcon
+						color="success"
+						cursor="pointer"
+						onClick={handleUpdateTodo}
+						size={16}
+					/>
+					<ClearIcon
+						color="error"
+						cursor="pointer"
+						onClick={handleCancelUpdate}
+						size={16}
+					/>
+				</div>
+			) : (
+				<div className="todo-item-container-action">
+					<EditIcon
+						sx={{ color: 'warning.light' }}
+						cursor="pointer"
+						onClick={() => {
+							console.log('edit mode');
+							setInEditMode(true);
+						}}
+						size={16}
+					/>
+					<DeleteIcon
+						color="error"
+						cursor="pointer"
+						onClick={() => {
+							props.onDeleteTodo(id);
+						}}
+						size={16}
+					/>
+				</div>
+			)}
 		</div>
 	);
 }
--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -22,12 +22,31 @@ function Todos() {
 	const [todos, setTodos] = useState([]);
 
 	const handleDeleteTodo = (id) => {
-		setTodos(todos.filter(todo => todo.id !== id))
-	}
+		setTodos(todos.filter((todo) => todo.id !== id));
+	};
+
+	const handleUpdateTodo = (updatedTodo) => {
+		const { id } = updatedTodo;
+		const existingTodoIndex = todos.findIndex((todo) => todo.id === id);
+		const removedOldTodo = todos.filter(todo => todo.id !== id)
+		// insert new todo in the index of the removed old todo
+		setTodos([
+			...removedOldTodo.slice(0, existingTodoIndex),
+			updatedTodo,
+			...removedOldTodo.slice(existingTodoIndex, todos.length),
+		]);
+	};
 
 	const TodosList =
 		todos.length > 0 ? (
-			todos.map((todo) => <TodoItem key={todo.id} item={todo} onDeleteTodo={handleDeleteTodo} />)
+			todos.map((todo) => (
+				<TodoItem
+					key={todo.id}
+					item={todo}
+					onDeleteTodo={handleDeleteTodo}
+					onUpdateTodo={handleUpdateTodo}
+				/>
+			))
 		) : (
 			<NoTodosMessage />
 		);
